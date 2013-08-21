3039add6608ee8db1de9778c0f192cb5454a7294
Tom Beigle <tbeigle@gmail.com>
2013-08-20 18:00:22 -0700

    Miscellaneous theme updates.

--- docroot/sites/all/themes/namu/scripts/js/main.js
+++ docroot/sites/all/themes/namu/scripts/js/main.js
@@ -1,7 +1,28 @@
 (function ($) {
   Drupal.behaviors.namuMain = {
     attach: function(context, settings) {
-      
+      if ($('.field-name-field-glossary-item').length) {
+        $('.field-name-field-glossary-item > .field-items > .field-item').each(function() {
+          var $this = $(this),
+              $term = $this.find('.field-name-field-glossary-term'),
+              $def = $this.find('.field-name-field-glossary-definition');
+          
+          $term.click(function() {
+            var is_vis = $def.hasClass('visible');
+            
+            $('.field-name-field-glossary-item > .field-items > .field-item .field-name-field-glossary-definition').slideUp(150);
+            
+            if (is_vis) {
+              $def.removeClass('visible');
+            }
+            else {
+              $def.slideDown(150, function() {
+                $def.addClass('visible');
+              });
+            }
+          });
+        });
+      }
     }
   }
 })(jQuery);
